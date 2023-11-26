25e2a30e1aaa797d4bbbda05df0b56a69975a01a
Kvetuse Husakova <DukeDeAlenson@gmail.com>
2023-11-26 18:26:52 +0100

    init commit
    Inicialization commit for project

---
+++ Source/JigSawPuzzle/JigSawPuzzleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JigSawPuzzleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JIGSAWPUZZLE_API AJigSawPuzzleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
